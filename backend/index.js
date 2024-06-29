8c117d2828bf2917d0b86f5bce98b49e2a5ec5e2
balajik27 <balajik272003@gmail.com>
2024-06-29 18:40:20 +0530

    mogodb url changed

--- backend/index.js
+++ backend/index.js
@@ -48,12 +48,12 @@ app.use('/wshcmForm', FORMS)
 //mongodb connection
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/ensureSafe')
+// mongoose.connect('mongodb://127.0.0.1:27017/ensureSafe')
+//   .then(() => console.log('Connected!'))
+
+
+  mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Connected!'))
-
-
-  // mongoose.connect(process.env.MONGO_URL)
-  // .then(() => console.log('Connected!'))
 
 
 const PORT = process.env.PORT || 3000;
