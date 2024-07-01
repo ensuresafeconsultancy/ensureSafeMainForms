626c99c1051308180b34bca065e187081f482ab9
balajik27 <balajik272003@gmail.com>
2024-07-01 10:28:58 +0530

    vite changes

--- frontend/courseForm/vite.config.js
+++ frontend/courseForm/vite.config.js
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['react-bootstrap'], // Exclude react-bootstrap from optimization
-  }
-})
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Add these lines for SPA build
+        entryFileNames: `assets/[name].[format].js`,
+        chunkFileNames: `assets/[name].[format].js`,
+        assetFileNames: (assetInfo) => {
+          // Optional: Customize asset filename generation here
+          return `assets/[name].[ext]`;
+        },
+      },
+    },
+    // Add this line if your app runs under a subdirectory on Render
+    // base: '/your-subdirectory-path/', // Adjust this if needed
+  },
+});
