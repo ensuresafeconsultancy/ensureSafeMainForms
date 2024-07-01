2cdc66d9796e02f3b5ab67b2d365c00ecad25f85
balajik27 <balajik272003@gmail.com>
2024-07-01 10:13:48 +0530

    added pages

--- frontend/courseForm/src/Home/homePage.jsx
+++ frontend/courseForm/src/Home/homePage.jsx
@@ -1,7 +1,16 @@
+import {Link} from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>Homepage</div>
+
+    <>
+
+        <div>Homepage</div>
+        <Link to="/WshcmForm">GO to WshcmForm </Link> <br />
+        <Link to="/admin">GO to Admin </Link>
+    
+    </>
+    
   )
 }
 
